0aed1aaa627162cfcb38b56cacda2db86937c329
Ali Shkeir <alishkeir_@hotmail.com>
2023-10-22 18:15:21 +0300

    Created Theme Template

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The footer for our theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fancy Lab
+ */
+
+?>
+
+<footer>
+    <section class="footer-widgets">Footer Widgets</section>
+    <section class="copyright">Copyright</section>
+</footer>
+</div>
+</body>
+
+</html>
